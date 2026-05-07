e57f5bc0b08f3e18fcb29a16862453a1aac0301c
Eugene Mendez <ecmendez1420@gmail.com>
2026-05-07 14:29:57 +0800

    added some minor navbar design and added the light and dark mode button and function

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,46 @@ import About from "./sections/About";
 import Projects from "./sections/Projects";
 import Experience from "./sections/Experience";
 import Contact from "./sections/Contact";
-import Navbar from "./layout/Navbar"; 
+import Navbar from "./layout/Navbar";
+import { createContext, useEffect, useState } from "react";
+
+export const BgContext = createContext();
 
 function App() {
+  const [background, setBackground] = useState("white");
+  const [icon, setIcon] = useState("Sun");
+
+  function handleBackgroundChange() {
+    setBackground((prev) => (prev === "white" ? "black" : "white"));
+    setIcon((prev) => (prev === "Sun" ? "Moon" : "Sun"));
+  }
+
+  useEffect(() => {
+    document.body.style.backgroundColor =
+      background === "white" ? "#ffffff" : "#000000";
+    document.body.style.color = background === "white" ? "#000000" : "#ffffff";
+  }, [background]);
+
   return (
     <>
-      <div className="min-h-screen overflow-x-hidden">
-        <Navbar />
-        <main>
-          <Hero />
-          <About />
-          <Projects />
-          <Experience />
-          <Contact />
-        </main>
-      </div>
+      <BgContext.Provider value={background}>
+        <div
+          className="min-h-screen overflow-x-hidden transition-colors"
+          style={{
+            backgroundColor: background === "white" ? "#ffffff" : "#000000",
+            color: background === "white" ? "#000000" : "#ffffff",
+          }}
+        >
+          <Navbar backgroundChange={handleBackgroundChange} bgIcon={icon} />
+          <main>
+            <Hero />
+            <About />
+            <Projects />
+            <Experience />
+            <Contact />
+          </main>
+        </div>
+      </BgContext.Provider>
     </>
   );
 }
--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,6 +1,7 @@
-import { Menu, X } from "lucide-react";
+import { Menu, Moon, Sun, X } from "lucide-react";
 import Button from "../components/Button";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { BgContext } from "../App";
 
 const navLinks = [
   {
@@ -20,25 +21,34 @@ const navLinks = [
     label: "Contact",
   },
 ];
-const Navbar = () => {
+const Navbar = ({ backgroundChange, bgIcon }) => {
   const [isMobile, setIsMobile] = useState(false);
+  const background = useContext(BgContext);
   return (
-    <header className="fixed top-0 left-0 right-0 bg-transparent py-5">
-      <nav className="container mx-auto px-6 flex items-center justify-between ">
+    <header className="fixed top-0 left-0 right-0 bg-transparent py-5 z-100">
+      <nav className="container mx-auto px-6 flex items-center justify-between font-bold uppercase">
         <a
           href="#"
-          className="text-xl font-bold tracking-tight transition-colors hover:text-primary"
+          className={
+            background === "white"
+              ? "text-xl font-bold tracking-tight transition-colors hover:text-primary-dark"
+              : "text-xl font-bold tracking-tight transition-colors hover:text-primary-light"
+          }
         >
           EM <span className="text-primary-text">.</span>
         </a>
         {/*Desktop Nav */}
-        <div className="hidden md:flex items-center gap-1">
+        <div className="hidden md:flex items-center gap-4 pl-30">
           <div className="glass rounded-full px-2 py-1 flex items-center gap-1">
             {navLinks.map((link, idx) => (
               <a
                 href={link.href}
                 key={idx}
-                className="px-4 py-2 text-sm text-muted-text hover:text-active-text rounded-full hover:bg-surface"
+                className={
+                  background === "white"
+                    ? "flex px-4 py-2 text-sm text-black hover:text-active-text-light rounded-full hover:bg-surface-dark"
+                    : "px-4 py-2 text-sm text-white hover:text-active-text-dark rounded-full hover:bg-surface-light"
+                }
               >
                 {link.label}
               </a>
@@ -47,33 +57,92 @@ const Navbar = () => {
         </div>
 
         {/*CTA Button */}
-        <div className="hidden md:block">
-          <Button size="sm">Contact Me</Button>
+        <div className="flex gap-6 justify-center content-center">
+          <div className="hidden md:block">
+            <Button
+              size="sm"
+              className={
+                background === "white"
+                  ? "bg-primary-light border-black"
+                  : "bg-primary-dark border-white"
+              }
+            >
+              Contact Me
+            </Button>
+          </div>
+          {/* Background Button */}
+          {bgIcon === "Sun" ? (
+            <Sun
+              className="cursor-pointer w-10 h-8 mt-1.5 hidden md:block hover:text-primary-dark"
+              onClick={() => backgroundChange()}
+            ></Sun>
+          ) : (
+            <Moon
+              className="cursor-pointer w-10 h-8 mt-1.5 hidden md:block  hover:text-primary-light"
+              onClick={() => backgroundChange()}
+            ></Moon>
+          )}
         </div>
-
         {/*Mobile Navigation Button */}
         <button
-          className="md:hidden p-2 text-background-text cursor-pointer"
+          className={
+            background === "white"
+              ? "md:hidden p-2 text-black cursor-pointer"
+              : "md:hidden p-2 text-white cursor-pointer"
+          }
           onClick={() => setIsMobile(!isMobile)}
         >
           {isMobile ? <X /> : <Menu size={24} />}
         </button>
       </nav>
+
       {/*Mobile Navigation */}
       {isMobile && (
-        <div className="md:hidden glass-strong animate-fade-in">
+        <div
+          className={
+            background === "white"
+              ? "md:hidden glass-light animate-fade-in"
+              : "md:hidden glass-dark animate-fade-in"
+          }
+        >
           <div className="container mx-auto px-6 py-6 flex flex-col gap-4">
             {navLinks.map((link, idx) => (
               <a
                 href={link.href}
                 key={idx}
-                className="text-lg text-muted-text hover:text-active-text py-2"
+                className={
+                  background === "white"
+                    ? "text-lg text-black hover:text-active-text py-2 font-bold hover:opacity-[0.5] text-center"
+                    : "text-lg text-muted-text hover:text-active-text py-2 font-bold hover:opacity-[0.5] text-center"
+                }
                 onClick={() => setIsMobile(false)}
               >
                 {link.label}
               </a>
             ))}
-            <Button size="sm">Contact Me</Button>
+            <Button
+              size="sm"
+              className={
+                background === "white"
+                  ? "bg-primary-light border-black"
+                  : "bg-primary-dark border-white"
+              }
+            >
+              Contact Me
+            </Button>
+            <Button
+              size="sm"
+              className={
+                background === "white"
+                  ? "bg-primary-dark"
+                  : "bg-primary-light text-primary-text-light"
+              }
+              onClick={() => backgroundChange()}
+            >
+              {background === "white"
+                ? "Enable Dark Mode"
+                : "Enable Light Mode"}
+            </Button>
           </div>
         </div>
       )}
